e9047996d8e8ac6a8886b83009601eb2007debc0
Tamas Kurics <tamas.kurics@gmail.com>
2023-12-09 10:11:02 +0100

    day 9 - single file
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ src/day09ab.ml
@@ -0,0 +1,40 @@
+let read_lines (filename : string) : string list =
+  In_channel.with_open_text filename (In_channel.input_lines)
+
+
+let convert_to_intlist (line : string) : int list =
+  List.map int_of_string Str.(line |> split (regexp "[ \t]+"))
+
+
+let neville_interpolation (ys : int array) (x : int) =
+  let n = Array.length ys in
+  let table = Array.make_matrix n (n + 1) 0 in
+  for k = 0 to n - 1 do
+    table.(k).(0) <- k - x;
+    table.(k).(1) <- ys.(k)
+  done;
+
+  for j = 2 to n do
+    for i = 0 to n - j do
+      let d = table.(i).(j-1) * table.(i+j-1).(0) - table.(i+1).(j-1) * table.(i).(0) in
+      table.(i).(j) <- d / (table.(i+j-1).(0) - table.(i).(0))
+    done;
+  done;
+  table.(0).(n)
+
+
+let sum_of_right_extrapolated_values (histories : int list list) : int =
+  List.fold_left (fun acc h -> acc + neville_interpolation (Array.of_list h) (List.length h)) 0 histories
+
+
+let sum_of_left_extrapolated_values (histories : int list list) : int =
+  List.fold_left (fun acc h -> acc + neville_interpolation (Array.of_list h) (-1)) 0 histories
+
+
+let () =
+  let lines = read_lines "../resources/input_09.txt" in
+  let histories = List.map convert_to_intlist lines in
+  let result_a = sum_of_right_extrapolated_values histories in
+  let result_b = sum_of_left_extrapolated_values histories in
+  print_int result_a; print_newline ();
+  print_int result_b; print_newline ()
