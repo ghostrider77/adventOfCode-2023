5206e31fc1c13d1074c9c1412362761e039766fd
Tamas Kurics <tamas.kurics@gmail.com>
2023-12-12 16:29:17 +0100

    refactor: string is the best representation of the spring conditions
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- src/day12b.ml
+++ src/day12b.ml
@@ -1,70 +1,61 @@
-module Array = struct
-  include Array
+module String = struct
+  include String
 
-  let drop_while p arr =
-    match find_index (fun x -> not (p x)) arr with
-      | Some ix -> sub arr ix (length arr - ix)
-      | None -> [||]
+  let drop_while p str =
+    str |> to_seq |> Seq.drop_while p |> of_seq
 
-  let span p arr =
-    match find_index (fun x -> not (p x)) arr with
-      | None -> (arr, [||])
-      | Some ix -> (sub arr 0 ix, sub arr ix (length arr - ix))
+  let span p str =
+    match Seq.find_index (fun c -> not (p c)) (to_seq str) with
+      | Some ix -> (sub str 0 ix, sub str ix (length str - ix))
+      | None -> (str, "")
+
 end
 
-type spring = Operational | Damaged | Unknown
-type record = { arrangement : spring array; group_sizes : int list }
-
-let spring_of_char = function
-  | '.' -> Operational
-  | '#' -> Damaged
-  | '?' -> Unknown
-  | _ -> failwith "Unknown spring condition."
-
+type record = { arrangement : string; group_sizes : int list }
 
 let parse_input_records (lines : string list) : record list =
   let parse line =
-    let spring_conditions, groups = Scanf.sscanf line "%s %s" (fun a g -> a, g) in
-    let arrangement = spring_conditions |> String.to_seq |> Seq.map spring_of_char |> Array.of_seq in
+    let arrangement, groups = Scanf.sscanf line "%s %s" (fun a g -> a, g) in
     let group_sizes = groups |> String.split_on_char ',' |> List.map int_of_string in
     { arrangement; group_sizes } in
 
   let unfold_record { arrangement; group_sizes } =
     let unfolded_sizes = List.flatten @@ List.init 5 (fun _ -> group_sizes) in
-    let unfolded_arrangement = List.tl @@ List.flatten @@ List.init 5 (fun _ -> Unknown :: Array.to_list arrangement) in
-    { arrangement = Array.of_list unfolded_arrangement; group_sizes = unfolded_sizes } in
+    let unfolded_arrangement = String.concat "?" @@ List.init 5 (fun _ -> arrangement) in
+    { arrangement = unfolded_arrangement; group_sizes = unfolded_sizes } in
 
   List.map (fun line -> unfold_record (parse line)) lines
 
 
 let different_arrangements (record : record) : int =
-  let change_first_occurrence arr s =
-    match Array.find_index (fun x -> x = Unknown) arr with
+  let change_first_occurrence str chr =
+    match String.index_opt str '?' with
       | Some ix ->
-          let arr' = Array.copy arr in
-          arr'.(ix) <- s;
-          arr'
-      | None -> arr in
+          let bs = String.to_bytes str in
+          Bytes.set bs ix chr;
+          Bytes.to_string bs
+      | None -> str in
+
   let cache = Hashtbl.create 1000 in
   let rec calc_arrangements {arrangement; group_sizes} =
-    let arrangement' = Array.drop_while (fun s -> s = Operational) arrangement in
-    match (Array.length arrangement', group_sizes) with
+    let arrangement' = String.drop_while (fun s -> s = '.') arrangement in
+    match (String.length arrangement', group_sizes) with
       | (0, []) -> 1
       | (0, _) -> 0
-      | (_ , []) -> if Array.for_all (fun s -> s <> Damaged) arrangement' then 1 else 0
+      | (_ , []) -> if String.for_all (fun s -> s <> '#') arrangement' then 1 else 0
       | (_, size :: sizes) ->
           let current_record = {arrangement = arrangement'; group_sizes} in
           match Hashtbl.find_opt cache current_record with
             | Some n -> n
             | None ->
-                let block, rest = Array.span (fun s -> s <> Operational) arrangement' in
+                let block, rest = String.span (fun s -> s <> '.') arrangement' in
                 let result =
-                  if Array.for_all (fun s -> s = Damaged) block then
-                    if Array.length block = size then calc_arrangements {arrangement = rest; group_sizes = sizes}
+                  if String.for_all (fun s -> s = '#') block then
+                    if String.length block = size then calc_arrangements {arrangement = rest; group_sizes = sizes}
                     else 0
                   else
-                    let a1 = change_first_occurrence arrangement' Operational in
-                    let a2 = change_first_occurrence arrangement' Damaged in
+                    let a1 = change_first_occurrence arrangement' '.' in
+                    let a2 = change_first_occurrence arrangement' '#' in
                     (calc_arrangements {arrangement = a1; group_sizes} +
                       calc_arrangements {arrangement = a2; group_sizes}) in
                 Hashtbl.add cache current_record result;
