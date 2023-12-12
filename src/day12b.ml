e17d92dca6b41cdfb1a65ddf656fc875b62fc671
Tamas Kurics <tamas.kurics@gmail.com>
2023-12-12 14:45:51 +0100

    refactor: use arrays instead of lists
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>
    
    Not that nice, but the running time went down from 6 minutes to 10 seconds.

--- src/day12b.ml
+++ src/day12b.ml
@@ -1,5 +1,19 @@
+module Array = struct
+  include Array
+
+  let drop_while p arr =
+    match find_index (fun x -> not (p x)) arr with
+      | Some ix -> sub arr ix (length arr - ix)
+      | None -> [||]
+
+  let span p arr =
+    match find_index (fun x -> not (p x)) arr with
+      | None -> (arr, [||])
+      | Some ix -> (sub arr 0 ix, sub arr ix (length arr - ix))
+end
+
 type spring = Operational | Damaged | Unknown
-type record = { arrangement : spring list; group_sizes : int list }
+type record = { arrangement : spring array; group_sizes : int list }
 
 let spring_of_char = function
   | '.' -> Operational
@@ -11,44 +25,42 @@ let spring_of_char = function
 let parse_input_records (lines : string list) : record list =
   let parse line =
     let spring_conditions, groups = Scanf.sscanf line "%s %s" (fun a g -> a, g) in
-    let arrangement = spring_conditions |> String.to_seq |> Seq.map spring_of_char |> List.of_seq in
+    let arrangement = spring_conditions |> String.to_seq |> Seq.map spring_of_char |> Array.of_seq in
     let group_sizes = groups |> String.split_on_char ',' |> List.map int_of_string in
     { arrangement; group_sizes } in
 
   let unfold_record { arrangement; group_sizes } =
     let unfolded_sizes = List.flatten @@ List.init 5 (fun _ -> group_sizes) in
-    let unfolded_arrangement = List.tl @@ List.flatten @@ List.init 5 (fun _ -> Unknown :: arrangement) in
-    { arrangement = unfolded_arrangement; group_sizes = unfolded_sizes } in
+    let unfolded_arrangement = List.tl @@ List.flatten @@ List.init 5 (fun _ -> Unknown :: Array.to_list arrangement) in
+    { arrangement = Array.of_list unfolded_arrangement; group_sizes = unfolded_sizes } in
 
   List.map (fun line -> unfold_record (parse line)) lines
 
 
 let different_arrangements (record : record) : int =
-  let change_first_occurrence xs s =
-    let rec loop acc = function
-      | x :: xss ->
-          if x = Unknown then (List.rev (s :: acc)) @ xss
-          else loop (x :: acc) xss
-      | [] -> List.rev acc in
-    loop [] xs in
-
+  let change_first_occurrence arr s =
+    match Array.find_index (fun x -> x = Unknown) arr with
+      | Some ix ->
+          let arr' = Array.copy arr in
+          arr'.(ix) <- s;
+          arr'
+      | None -> arr in
   let cache = Hashtbl.create 1000 in
-  let open Batteries in
   let rec calc_arrangements {arrangement; group_sizes} =
-    let arrangement' = List.drop_while (fun s -> s = Operational) arrangement in
-    match (arrangement', group_sizes) with
-      | ([], []) -> 1
-      | ([], _) -> 0
-      | (_ , []) -> if List.for_all (fun s -> s <> Damaged) arrangement' then 1 else 0
+    let arrangement' = Array.drop_while (fun s -> s = Operational) arrangement in
+    match (Array.length arrangement', group_sizes) with
+      | (0, []) -> 1
+      | (0, _) -> 0
+      | (_ , []) -> if Array.for_all (fun s -> s <> Damaged) arrangement' then 1 else 0
       | (_, size :: sizes) ->
           let current_record = {arrangement = arrangement'; group_sizes} in
-          match Hashtbl.find_option cache current_record with
+          match Hashtbl.find_opt cache current_record with
             | Some n -> n
             | None ->
-                let block, rest = List.span (fun s -> s <> Operational) arrangement' in
+                let block, rest = Array.span (fun s -> s <> Operational) arrangement' in
                 let result =
-                  if List.for_all (fun s -> s = Damaged) block then
-                    if List.length block = size then calc_arrangements {arrangement = rest; group_sizes = sizes}
+                  if Array.for_all (fun s -> s = Damaged) block then
+                    if Array.length block = size then calc_arrangements {arrangement = rest; group_sizes = sizes}
                     else 0
                   else
                     let a1 = change_first_occurrence arrangement' Operational in
