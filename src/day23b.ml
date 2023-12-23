46580856b708e098feba1afe3b26c46b33751afd
Tamas Kurics <tamas.kurics@gmail.com>
2023-12-23 17:51:54 +0100

    refactor: use the junction graph only for part 2 in day 23
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- src/day23b.ml
+++ src/day23b.ml
@@ -1,6 +1,7 @@
 type coord = { x : int; y : int }
 type tile = Trail | Forest
 type hiking_map = { grid : tile array array; nrows : int; ncols : int }
+type edge = { coord : coord; weight : int }
 
 module CoordSet = Set.Make(
   struct
@@ -8,7 +9,13 @@ module CoordSet = Set.Make(
     let compare = Stdlib.compare
   end)
 
-type path = { head : coord; previous : coord; visited_junctions : CoordSet.t; length : int}
+module CoordMap = Map.Make(
+  struct
+    type t = coord
+    let compare = Stdlib.compare
+  end)
+
+type path = { head : coord; visited : CoordSet.t; length : int }
 
 
 let tile_of_char = function
@@ -34,32 +41,50 @@ let get_neighbors ({grid; nrows; ncols} : hiking_map) ({x; y} : coord) : coord l
   let potential_neighbors =
     match grid.(x).(y) with
       | Trail -> [{x = x - 1; y}; {x; y = y + 1}; {x = x + 1; y}; {x; y = y - 1}]
-      | Forest -> failwith "One cannot enter the forest." in
+      | Forest -> [] in
   List.filter is_valid potential_neighbors
 
 
-let find_all_paths (map : hiking_map) (start : coord) (target : coord) : int =
-  let rec grow ({head; previous; visited_junctions; length} as path) =
-    match List.filter (fun n -> n <> previous) (get_neighbors map head) with
-      | [] -> []
-      | [next] ->
-          if next = target then [{path with head = next; previous = head; length = length + 1}]
-          else grow {path with head = next; previous = head; length = length + 1}
-      | neighbors ->
-          if CoordSet.mem head visited_junctions then []
-          else
-            let js' = CoordSet.add head visited_junctions in
-            List.map (fun n -> {head = n; previous = head; visited_junctions = js'; length = length + 1}) neighbors in
+let find_junctions ({nrows; ncols; _} as map : hiking_map) : CoordSet.t =
+  let coords = Seq.flat_map (fun x -> Seq.map (fun y -> {x; y}) (Seq.init ncols Fun.id)) (Seq.init nrows Fun.id) in
+  let is_junction coord =
+    let neighbors = get_neighbors map coord in
+    List.length neighbors >= 3 in
+
+  let junctions = Seq.filter is_junction coords in
+  CoordSet.of_seq junctions
+
+
+let create_junction_graph (map : hiking_map) (start : coord) (target : coord) : edge list CoordMap.t =
+  let junctions = map |> find_junctions |> CoordSet.add start |> CoordSet.add target in
+  let find_edges coord =
+    let rec extend {head; visited; length} =
+      match List.filter (fun n -> not (CoordSet.mem n visited)) (get_neighbors map head) with
+        | [neighbor] ->
+            if CoordSet.mem neighbor junctions then Some {coord = neighbor; weight = length + 1}
+            else extend {head = neighbor; visited = CoordSet.add head visited; length = length + 1}
+        | _ -> None in
+    let neighbors = get_neighbors map coord in
+    List.filter_map (fun n -> extend {head = n; visited = CoordSet.singleton coord; length = 1}) neighbors in
+
+  CoordSet.fold (fun coord acc -> let ns = find_edges coord in CoordMap.add coord ns acc) junctions CoordMap.empty
+
+
+let calc_length_of_longest_trail (graph : edge list CoordMap.t) (start : coord) (target : coord) : int =
+  let grow {head; visited; length} =
+    let neighbors = List.filter (fun {coord; _} -> not (CoordSet.mem coord visited)) (CoordMap.find head graph) in
+    List.map (fun {coord; weight} -> {head = coord; visited = CoordSet.add head visited; length = length + weight})
+      neighbors in
 
   let rec loop acc = function
     | [] -> acc
     | paths ->
         let paths_to_target, growing_paths = List.partition (fun {head; _} -> target = head) paths in
-        let longest = List.fold_left (fun acc' {length; _} -> max acc' length) 0 paths_to_target in
+        let longest = List.fold_left (fun acc {length; _} -> max length acc) 0 paths_to_target in
         let paths' = List.concat_map grow growing_paths in
         loop (max acc longest) paths' in
 
-  loop 0 [{head = start; previous = start; visited_junctions = CoordSet.empty; length = 0}]
+  loop 0 [{head = start; visited = CoordSet.empty; length = 0}]
 
 
 let find_longest_trail ({grid; nrows; _} as map : hiking_map) : int =
@@ -73,7 +98,8 @@ let find_longest_trail ({grid; nrows; _} as map : hiking_map) : int =
       | Some coord -> coord in
   let start = {x = 0; y = start_column} in
   let target = {x = nrows - 1; y = target_column} in
-  find_all_paths map start target
+  let graph = create_junction_graph map start target in
+  calc_length_of_longest_trail graph start target
 
 
 let () =
